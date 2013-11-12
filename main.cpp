876b26c4b544d26364f64c188f8eb87db45fc104
BostjanSkok <BostjanSkok2@gmail.com>
2013-11-12 19:00:03 +0100

    done

--- main.cpp
+++ main.cpp
@@ -4,74 +4,151 @@
 #include <stdlib.h>
 
 
-const int M = 20;
-const int N = 4;
-
+const int M = 16;
+const int N = 2;
+pthread_mutex_t locks[N];
 pthread_barrier_t barrier;
 
 int vekt1[M];
 int vekt2[M];
 int result[N];
 
-int r;
+
 
 void FillVektors(){
 	for (int i = 0; i < M; i++)
 	{
-		vekt1[i] = rand()%10;
-		vekt2[i] = rand()%10;
+		//vekt1[i] = rand()%10;
+		//vekt2[i] = rand()%10;
+		vekt1[i] = 1;
+		vekt2[i] = 2;
 	}
+}
+
+void printVector()
+{
+	for (int i = 0; i < N; i++)
+	{
+		printf("%i;", result[i]);
+	}
+	printf("\n");
+}
+
+
+
+void* DoWork2(void * arg){
+	int threadId = (int)arg;
+	int start = threadId*(M / N);
+	int sum = 0;
+	int stepSize = 2;
+	for (int i = start; i < (threadId + 1) * (M / N); i++)
+	{
+		sum += vekt1[i] * vekt2[i];
+	}
+
+	result[threadId] = sum;
+	
+	pthread_barrier_wait(&barrier);
+	//if (threadId == 0){ printVector(); }
+	//pthread_barrier_wait(&barrier);
+
+	while (stepSize <= N)
+	{
+
+		if (threadId % stepSize == 0 && threadId + (stepSize / 2) < N){
+			//	printf("thread %i", threadId);
+			result[threadId] += result[threadId + (stepSize / 2)];
+			//printf(" value = %i\n", result[threadId]);
+		}
+		stepSize *= 2;
+		pthread_barrier_wait(&barrier);
+	
+		//if (threadId == 0){ printVector(); }
+		//pthread_barrier_wait(&barrier);
+	}
+
+
+
+	return NULL;
 }
 
 void* DoWork(void * arg){
 	int threadId = (int)arg;
 	int start = threadId*(M / N);
 	int sum = 0;
-	int counter = 2;
-	for (int i =start; i < threadId + (M / N); i++)
+	int stepSize = 2;
+	for (int i = start; i < (threadId + 1) * (M / N); i++)
 	{
-		sum += vekt1[i] + vekt2[i];
+		sum += vekt1[i] * vekt2[i];
 	}
 
-	result[threadId]=sum;
-	pthread_barrier_wait(&barrier);
-	while (counter<N)
+	result[threadId] = sum;
+
+
+	//if (threadId == 0){ printVector(); }
+	//pthread_barrier_wait(&barrier);
+
+	while (stepSize <= N)
 	{
-		if (threadId % counter == 1){
-			result[threadId] += result[threadId + 1];
+
+		if (threadId % stepSize == 0 && threadId + (stepSize / 2) < N){
+			//	printf("thread %i", threadId);
+			pthread_mutex_lock(&locks[threadId + (stepSize / 2)]);
+			
+			result[threadId] += result[threadId + (stepSize / 2)];
+
+			pthread_mutex_unlock(&locks[threadId + (stepSize / 2)]);
+
+			//printf(" value = %i\n", result[threadId]);
 		}
-		counter * 2;
-		pthread_barrier_wait(&barrier);
+		else
+		{
+			pthread_mutex_unlock(&locks[threadId]);
+		}
+		stepSize *= 2;
+
+		//if (threadId == 0){ printVector(); }
+		//pthread_barrier_wait(&barrier);
 	}
 
-
-	
 
 
 	return NULL;
 }
-
-
 int main() {
 
 	pthread_t threads[N];
 	srand(time(NULL));
 	
+
 	FillVektors();
 
+	for (int i = 0; i < N; i++){
+		pthread_mutex_init(&locks[i], NULL);
+	}
+
 	pthread_barrier_init(&barrier, NULL, N);
-	for (int i=0; i < N; i++){
-		pthread_create(&threads[i],NULL,DoWork,(void*) i);
+	for (int i = 0; i < N; i++){
+		pthread_mutex_lock(&locks[i]);
+		pthread_create(&threads[i], NULL, DoWork, (void*)i);
 	}
 
-	for (int i=0; i < N; i++){
-		pthread_join(threads[i],NULL);
+	for (int i = 0; i < N; i++){
+		pthread_join(threads[i], NULL);
 	}
 
+	printf("%i \n", result[0]);
+	getchar();
 	pthread_barrier_destroy(&barrier);
 
 
 
-
 }
+
+
+
+
+
+
+
 
